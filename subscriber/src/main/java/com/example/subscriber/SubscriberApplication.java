58cc9d0dc1f4b4eda7f33ebc899e6bdacd6311b9
asliddin <asliddintojaliyev88@gmail.com>
2022-10-13 01:06:32 +0500

    commit

--- subscriber/src/main/java/com/example/subscriber/SubscriberApplication.java
+++ subscriber/src/main/java/com/example/subscriber/SubscriberApplication.java
@@ -1,10 +1,12 @@
 package com.example.subscriber;
 
+import lombok.*;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -13,6 +15,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 @SpringBootApplication
 @EnableRabbit
@@ -21,7 +27,6 @@ public class SubscriberApplication {
     public static void main(String[] args) {
         SpringApplication.run(SubscriberApplication.class, args);
     }
-
 }
 
 
@@ -61,5 +66,27 @@ class RabbitMQConfig {
         rabbitTemplate.setMessageConverter(messageConverter());
         return rabbitTemplate;
     }
+
+}
+
+@Component
+class TransactionListener {
+
+    @RabbitListener(queues = RabbitMQConfig.QUEUE)
+    public void logTransaction(Transaction transaction) {
+        System.out.println(transaction);
+    }
+}
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+class Transaction {
+    private Long id;
+    private String pan;
+    private BigDecimal amount;
+    private Timestamp createdAt;
 
 }

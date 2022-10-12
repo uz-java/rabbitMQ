9e24e5f0f4a1da373ed2dfd590067f041438b41d
asliddin <asliddintojaliyev88@gmail.com>
2022-10-13 01:03:05 +0500

    commit

--- subscriber/src/main/java/com/example/subscriber/SubscriberApplication.java
+++ subscriber/src/main/java/com/example/subscriber/SubscriberApplication.java
@@ -1,8 +1,18 @@
 package com.example.subscriber;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableRabbit
@@ -10,6 +20,46 @@ public class SubscriberApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SubscriberApplication.class, args);
+    }
+
+}
+
+
+@Configuration
+class RabbitMQConfig {
+    public static final String QUEUE = "pdp_queue";
+    public static final String EXCHANGE = "pdp_exchange";
+    public static final String ROUTING_KEY = "pdp_routing_key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
     }
 
 }
